06b67e3ab785d0a0b74aa3a242b26c8020858f45
Kshitij Gupta <kshitijgm@gmail.com>
2021-03-28 21:11:49 +0530

    Introduce Lawnchair 11 Alpha 5
    - An Android 11 Quickstep Launcher that does not suck.
    
    gotenksIN edit: move to standalone repo and convert to RRO
                    re-categorize everything and switch to Blueprint
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

---
+++ lawnchair.mk
@@ -0,0 +1,9 @@
+# Lawnchair
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/lawnchair
+
+PRODUCT_PACKAGES += \
+    Lawnchair \
+    LawnchairOverlay \
+    lawnchair-hiddenapi-package-whitelist \
+    privapp-permissions-lawnchair
